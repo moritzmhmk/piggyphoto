bc8702fc96c8294d61d6af313c031428760c31bc
enkore <public@enkore.de>
2013-01-31 19:24:35 +0100

    flying

--- piggyphoto/ptph.py
+++ piggyphoto/ptph.py
@@ -2,10 +2,9 @@
 
 import re
 
-with open("ptp.h") as inp:
-    with open("ptp.py", "w") as out:
-        out.write("# Constants extracted from gphoto2's ptp.h\n\n")
+with open("ptp.h") as inp, open("ptp.py", "w") as out:
+    out.write("# Constants extracted from gphoto2's ptp.h\n\n")
 
-        re.sub(r"#define[ \t]+([a-zA-Z0-9_]+)[ \t]+([^\n]*)\n",
-            lambda match: str(out.write("%s = %s\n" % (match.groups()[0], match.groups()[1].replace("/*", "#"))))
-            , inp.read())
+    re.sub(r"#define[ \t]+([a-zA-Z0-9_]+)[ \t]+([^\n]*)\n",
+        lambda match: str(out.write("%s = %s\n" % (match.groups()[0], match.groups()[1].replace("/*", "#"))))
+        , inp.read())
