3a93d6d6db0e0a590b15c1bf9daccec3ea157e61
enkore <public@enkore.de>
2013-01-31 18:25:03 +0100

    Nobody likes loops :>

--- piggyphoto/ptph.py
+++ piggyphoto/ptph.py
@@ -2,15 +2,10 @@
 
 import re
 
-regex = re.compile(r"^#define\s+([a-zA-Z0-9_]+)\s+(.*)")
+with open("ptp.h") as inp:
+    with open("ptp.py", "w") as out:
+        out.write("# Constants extracted from gphoto2's ptp.h\n\n")
 
-with open("ptp.py", "w") as out:
-    out.write("# Constants extracted from gphoto2's ptp.h\n\n")
-
-    with open("ptp.h") as inp:
-        for line in (line.strip() for line in inp.readlines() if line.startswith("#define")):
-            match = regex.match(line)
-            if match:
-                name, value = match.groups()
-                value = value.replace("/*", "#").replace("//", "#")
-                out.write("%s = %s\n" % (name, value))
+        re.sub(r"#define[ \t]+([a-zA-Z0-9_]+)[ \t]+([^\n]*)\n",
+            lambda match: str(out.write("%s = %s\n" % (match.groups()[0], match.groups()[1].replace("/*", "#"))))
+            , inp.read())
